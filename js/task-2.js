f926642636bcb1aec5fe21365f02dbc5ff520e17
VadiDem <demochko.vadim@gmail.com>
2024-01-16 01:04:11 +0200

    last fix 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,39 +1,42 @@
-const images = [
-  {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-  {
-    url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Elephant Beside on Baby Elephant",
-  },
-  {
-    url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Blue Geeen and Orange Parrot",
-  },
-  {
-    url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Zebras on Zebra",
-  },
-];
-const gallery = document.querySelector(".gallery");
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    {
+      url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "White and Black Long Fur Cat",
+    },
+    {
+      url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    },
+    {
+      url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      alt: "Group of Horses Running",
+    },
+    {
+      url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Elephant Beside on Baby Elephant",
+    },
+    {
+      url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Blue Green and Orange Parrot",
+    },
+    {
+      url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      alt: "Zebras on Zebra",
+    },
+  ];
 
-images.forEach((image) => {
-  const listItem = document.createElement("li");
-  listItem.classList.add("gallery-item");
+  const galleryList = document.getElementById("gallery");
 
-  const img = document.createElement("img");
-  img.src = image.url;
-  img.alt = image.alt;
+  images.forEach((image) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery-item");
 
-  listItem.appendChild(img);
-  gallery.appendChild(listItem);
+    const img = document.createElement("img");
+    img.src = image.url;
+    img.alt = image.alt;
+
+    galleryItem.appendChild(img);
+    galleryList.appendChild(galleryItem);
+  });
 });
